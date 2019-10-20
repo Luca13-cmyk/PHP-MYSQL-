a10f8abf65fa5feff3d174bf3d5ba8bd1e12a837
Luca Negrette <lucanegrette@hotmail.com>
2019-10-19 21:59:04 -0700

    aasdas

--- DAO (Data Acess Object)/index.php
+++ DAO (Data Acess Object)/index.php
@@ -29,9 +29,7 @@ require_once("config.php");
 // Lista de usuarios
 
 
-$lista =  Usuario::getList("tb_usuarios");
 
-echo json_encode($lista);
 
 
 // carrega uma lista de usarios buscando pelo login
@@ -48,9 +46,23 @@ echo json_encode($lista);
 
 
 
+// $conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "");
+// $stmt = $conn->prepare("INSERT INTO tb_usuarios (deslogin, dessenha) VALUES(:LOGIN, :PASSWORD)");
+
+// $login = "jose";
+// $password = "12333545689";
+
+// $stmt->bindParam(":LOGIN", $login);
+// $stmt->bindParam(":PASSWORD", $password);
+// $stmt->execute();
+
+// echo "Inserido OK";
 
 
 
+$lista =  Usuario::getList("tb_usuarios");
+
+echo json_encode($lista);
 // echo $root;
 
 
